66788b1a1e7606c5c4485e1e40635e38e76285d5
Jaime <jaimemoralmillan@digitechfp.com>
2024-05-21 09:56:08 +0200

    agregadas franjas horarias
    se crean y agregan a la base de datos las franjas horarias por defectos a la tabla franjahoraria

---
+++ monitorizacion_electrica/src/main/java/com/proyecto_a/negocio/Inicializacion.java
@@ -0,0 +1,16 @@
+package com.proyecto_a.negocio;
+
+import com.proyecto_a.dao.FranjaHorariaDAO;
+import com.proyecto_a.dto.FranjaHoraria;
+
+public class Inicializacion {
+    
+    public FranjaHorariaDAO dao = new FranjaHorariaDAO(); // Instancia de la clase FranjaHorariaDAO.
+        public static FranjaHoraria[] franjas = { // Array de franjas horarias.
+            new FranjaHoraria("Madrugada", "00:00:00", "06:00:00"),
+            new FranjaHoraria("Mañana", "06:00:00", "08:00:00"),
+            new FranjaHoraria("Mañana/Tarde", "08:00:00", "14:00:00"),
+            new FranjaHoraria("Tarde/Noche", "14:00:00", "18:00:00"),
+            new FranjaHoraria("Noche", "18:00:00", "24:00:00")
+        };
+}
