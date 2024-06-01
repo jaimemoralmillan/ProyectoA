df5945042fea0171f22bb6f396a9eddb39ce9903
Nick <nicklasperez@digitechfp.com>
2024-06-01 12:46:54 +0200

    Mostrar Categoria Y Franjas Casi terminado
    Cuando pulsas mostrar categoria te muestra las franjas, solo tengo que cambiar para que salgan las horas y el nombre de la pestaña

--- monitorizacion_electrica/src/main/java/com/proyecto_a/dto/Dispositivos_has_franjaHoraria.java
+++ monitorizacion_electrica/src/main/java/com/proyecto_a/dto/Dispositivos_has_franjaHoraria.java
@@ -5,7 +5,18 @@ public class Dispositivos_has_franjaHoraria {
     private int idFranjaHoraria;
     private int idDispositivo;
     private String nivelPrioridad;
+    private String descripcion;
 
+    public Dispositivos_has_franjaHoraria(String descripcion, String nivelPrioridad) {
+        this.descripcion = descripcion;
+        this.nivelPrioridad = nivelPrioridad;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     public static String optimo = "Óptima";
     public static String aceptable = "Aceptable";
     public static String mala = "Mala";
@@ -28,5 +39,10 @@ public class Dispositivos_has_franjaHoraria {
     }
     public void setNivelPrioridad(String nivelPrioridad) {
         this.nivelPrioridad = nivelPrioridad;
+    }
+
+    @Override
+    public String toString() {
+        return "Franja Horaria: " + descripcion + ", Nivel de Prioridad: " + nivelPrioridad; // Devuelve la descripción y el nivel de prioridad de la franja horaria.
     }
 }
